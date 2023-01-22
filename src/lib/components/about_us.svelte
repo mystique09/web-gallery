da7afa04c9ef51afa6daf07aa17baba89f7432c4
mystique09 <knightkrypto7@gmail.com>
2023-01-22 16:54:59 +0800

    add: components

---
+++ src/lib/components/about_us.svelte
@@ -0,0 +1,5 @@
+<div class="container container-lg min-h-screen  m-auto">
+	<div class="w-full px-4">
+		<h1>About us</h1>
+	</div>
+</div>
---
+++ src/lib/components/home.svelte
@@ -0,0 +1,5 @@
+<div class="container container-lg min-h-screen  m-auto">
+	<div class="w-full px-4">
+		<h1>Welcome to Brand name</h1>
+	</div>
+</div>
