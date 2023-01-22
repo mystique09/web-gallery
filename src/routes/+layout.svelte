23753e7e22ef05456d835281d0e4570d57740922
mystique09 <knightkrypto7@gmail.com>
2023-01-22 16:54:05 +0800

    add: layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import '../app.css';
+  import '@fontsource/poppins';
+	import Footer from '../lib/components/footer.svelte';
+	import Navbar from '../lib/components/navbar.svelte';
+</script>
+
+<Navbar />
+<slot />
+<Footer />
