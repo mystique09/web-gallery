f5049a8fe8ff789e55937b7b4ff35c9fa5385a5f
mystique09 <knightkrypto7@gmail.com>
2023-01-24 14:55:11 +0800

    feat: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script>
+	import Card from '$lib/components/card.svelte';
 	import Home from '$lib/components/home.svelte';
+	import { gallery } from '$lib/stores/animals';
+
+	let images = $gallery.map((image) => {
+		return {
+			id: image.id,
+			image: image.url,
+			description: image.description,
+			title: image.title
+		};
+	});
 </script>
 
 <Home />
+<div id="about" class="mb-12 container w-full">
+	<div class="w-full m-auto">
+		<div class="flex md:flex-row flex-wrap items-center justify-evenly w-full">
+			{#each images as image}
+				<Card card={image} />
+			{/each}
+		</div>
+	</div>
+</div>
