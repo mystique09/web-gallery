6b44cf45e464dda0c87d9c7526319faebe7f78b3
mystique09 <knightkrypto7@gmail.com>
2023-01-24 14:55:05 +0800

    feat: about page

--- src/routes/about/[uuid]/+page.svelte
+++ src/routes/about/[uuid]/+page.svelte
@@ -1,9 +1,26 @@
 <script>
 	import { page } from '$app/stores';
+	import { gallery } from '$lib/stores/animals';
+	let animalInfo = $gallery.find((animal) => animal.id === $page.params.uuid);
 </script>
 
 <div class="container container-lg min-h-screen  m-auto">
-	<div class="w-full px-4">
-		<h1>About {$page.params.uuid}</h1>
+	<div class="w-full px-4 my-12">
+		<figure class="md:w-1/2 m-auto">
+			<img
+				draggable="false"
+				class="w-full max-h-[420px] object-cover"
+				src={animalInfo?.url}
+				alt={animalInfo?.title}
+			/>
+			<h1 class="font-bold text-4xl mt-2 text-white">{animalInfo?.title}</h1>
+			<p class="text-sm mt-2">{animalInfo?.description}</p>
+			<a
+				target="_blank"
+				rel="noreferrer"
+				class="link link-primary text-xs"
+				href={animalInfo?.unsplash}>View original on Unsplash</a
+			>
+		</figure>
 	</div>
 </div>
