d2b9224bc36f33aca489584bcec9ea3d376304f5
mystique09 <knightkrypto7@gmail.com>
2023-01-24 14:54:59 +0800

    feat: contact-us page

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,5 +1,54 @@
-<div class="container container-lg min-h-screen  m-auto">
-	<div class="w-full px-4">
-		<h1>Contact us</h1>
+<div class="min-h-screen h-screen">
+	<div class="w-full max-w-6xl m-auto h-full">
+		<div class="flex flex-col items-center justify-center h-full">
+			<h1 class="text-xl md:text-6xl text-primary tracking-wider">CONTACT US</h1>
+			<p class="text-xs md:text-base text-center tracking-wide font-light">
+				We can help. Our team of experts is on hand to answer your questions.
+			</p>
+			<div class="flex flex-row gap-12 items-center justify-center mt-12 w-full">
+				<div class="email flex flex-col items-start">
+					<div class="flex flex-row items-center gap-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-8 stroke-secondary"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+							/>
+						</svg>
+						<span class="md:text-lg">Email us</span>
+					</div>
+					<a class="link mt-2 text-xs" href="mailto:myemail@email.com"
+						>myemail@email.com</a
+					>
+				</div>
+				<div class="call flex flex-col items-start">
+					<div class="flex flex-row items-center gap-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 stroke-secondary"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+							/>
+						</svg>
+						<span class="md:text-lg">Give us a call</span>
+					</div>
+					<p class="mt-2 text-xs">+63 123-456-7890</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
