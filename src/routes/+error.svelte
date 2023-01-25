130372998d2c6e9e69ec8ac65878c350656f2755
mystique09 <knightkrypto7@gmail.com>
2023-01-25 16:02:22 +0800

    add: custom error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="m-auto min-h-screen h-screen w-full">
+	<div class="flex flex-col items-center justify-center h-full">
+		<h1 class="text-lg font-light">Error {$page.status}</h1>
+		<p class="text-2xl text-error md:text-6xl">{$page.error?.message}</p>
+		<a class="link link-primary mt-2" href="/">Go back</a>
+	</div>
+</div>
